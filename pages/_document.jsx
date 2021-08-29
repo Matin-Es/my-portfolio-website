db614ab17f91e6eb2e9e95f3b7c175ed3002e5cf
Matin Esmaili <esmaili.matin82@gmail.com>
2021-08-29 18:37:10 +0430

    moved viewport meta tag to index.js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
         </Head>
         <body>
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = () => {
           name="description"
           content="Matin Esmaili Junior Reactjs Developer"
         />
-        
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
