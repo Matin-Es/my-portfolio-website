5a57e66e4c288b503540830d03dd5b30811c2bf8
Matin Esmaili <esmaili.matin82@gmail.com>
2021-08-29 15:46:18 +0430

    tailwindcss config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   purge: {
     content: [
       "./pages/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
+      "./components/*.{js,jsx,ts,tsx}",
     ],
   },
   darkMode: false, // or 'media' or 'class'
