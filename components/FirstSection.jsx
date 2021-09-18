a3cfee16fda029db970a4b447653fde678db010e
Matin Esmaili <esmaili.matin82@gmail.com>
2021-09-19 03:28:16 +0430

    SEO friendlied

--- components/FirstSection.jsx
+++ components/FirstSection.jsx
@@ -24,22 +24,11 @@ const FirstSection = () => {
               }}
             />
           </div>
-          <p className="mt-10 text-gray-600 text-xl hidden md:block lg:hidden ">
-            Hello folks, my name is Matin <br />
-            and I was born in March 2003 <br /> in Sari I started programming
-            <br />
-            since 2018 and I try to <br /> keep my skills up to date with
-            <br /> the new technologies
-          </p>
-          <p className="mt-10 text-gray-600 text-xl hidden lg:relative lg:left-0 lg:block sm:block sm:text-center md:hidden md:text-center lg:text-left sm:relative sm:right-20">
-            Hello folks, my name is Matin and I was born <br /> in March 2003 in
-            Sari <br /> I started programming since 2018 and I try to <br />{" "}
-            keep my skills up to date with the new <br /> technologies
-          </p>
-          <p className="mt-10 mr-44 text-gray-600 text-xl text-center block sm:hidden ">
-            Hello folks, my name is Matin and I was born in March 2003 in Sari I
-            started programming since 2018 and I try to keep my skills up to
-            date with the new technologies
+       
+          <p className="mt-10 mr-44 text-gray-600 text-xl text-center block md:text-left ">
+            Hello people, my name is Matin and <br/> I was born in March 2003 in Sari <br/> I
+            started programming since 2018 <br/> and I try to keep my skills up to
+            date <br/>with the new technologies
           </p>
           <div className="mt-6 ml-10 md:relative md:right-10">
             <a
