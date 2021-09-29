d5e4b2f6ab2a04df8c6c1755b6a2920be82f62cd
Matin Esmaili <esmaili.matin82@gmail.com>
2021-09-30 00:42:10 +0330

    another html tag added for google search console

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="google-site-verification" content="Xu1mg19rMHvQEGk6otoD1lHmjoMxuUruVX1tlym8wo0" />
         </Head>
         <body>
           <Main />
